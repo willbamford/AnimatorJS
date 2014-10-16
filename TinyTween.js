b02fc8507c05ae67f276f7708e9ff7a4034fb05d
Will Bamford <w.bamford@gmail.com>
2014-10-16 13:44:11 +0100

    Added primitive start / stop methods and event handling.

--- TinyTween.js
+++ TinyTween.js
@@ -1,8 +1,59 @@
 
 var TinyTween = function () {
-  
+
+  this.running = false;
+
+  this.listeners = {
+    start: [],
+    stop: []
+  };
 };
 
 TinyTween.create = function () {
   return new TinyTween();
 };
+
+TinyTween.prototype.on = function (type, fn) {
+  if (this.listeners[type] && this.listeners[type].indexOf(fn) === -1)
+    this.listeners[type].push(fn);
+  return this;
+};
+
+TinyTween.prototype.off = function (type, fn) {
+  if (this.listeners[type]) {
+    var i = this.listeners[type].indexOf(fn);
+    if (i !== -1) this.listeners[type].splice(i, 1);
+  }
+  return this;
+};
+
+TinyTween.prototype.start = function () {
+  if (!this.running) {
+    this.running = true;
+    this._notify('start', {});
+  }
+  return this;
+};
+
+TinyTween.prototype.stop = function () {
+  if (this.running) {
+    this.running = false;
+    this._notify('stop', {});
+  }
+};
+
+TinyTween.prototype._notify = function (type, event) {
+  event = event || {};
+  if (this.listeners[type]) {
+    this.listeners[type].forEach(function (fn) {
+      fn(event);
+    });
+  }
+};
+
+// TinyTween.prototype.off = function ()
+
+// var TinyTween.create({
+//   ease: TinyTween.LINEAR,
+//   duration: 1000
+// });
--- tests/TinyTweenSpec.js
+++ tests/TinyTweenSpec.js
@@ -1,13 +1,74 @@
-describe("TinyTween", function () {
+describe('TinyTween', function () {
 
   beforeEach(function () {
     /* */
   });
 
-  describe("create", function () {
-    it("should instantiate TinyTween", function () {
+  describe('create', function () {
+
+    it('should be able to instantiate a tween', function () {
       var tween = TinyTween.create();
       expect(tween instanceof TinyTween).toBeTruthy();
+    });
+  });
+
+  describe('on / off', function () {
+
+    it('should be able to subscribe / unsubscribe to valid event types', function () {
+      var tween = TinyTween.create();
+      var called = false;
+      var callback = function () { called = true; };
+      tween.on('start', callback);
+      expect(called).toBeFalsy();
+      tween.start();
+      expect(called).toBeTruthy();
+      called = false;
+      tween = TinyTween.create();
+      tween.on('start', callback);
+      tween.off('start', callback);
+      tween.start();
+      expect(called).toBeFalsy();
+    });
+  });
+
+  describe('start', function () {
+
+    it('should set the tween running', function () {
+      var tween = TinyTween.create();
+      expect(tween.running).toBeFalsy();
+      tween.start();
+      expect(tween.running).toBeTruthy();
+    });
+
+    it('should fire a "start" event once', function () {
+      var count = 0;
+      var tween = TinyTween.create().on('start', function (event) {
+        count += 1;
+      });
+      tween.start();
+      tween.start();
+      expect(count).toBe(1);
+    });
+  });
+
+  describe('stop', function () {
+
+    it('should result in tween no longer running', function () {
+      var tween = TinyTween.create();
+      tween.start();
+      expect(tween.running).toBeTruthy();
+      tween.stop();
+      expect(tween.running).toBeFalsy();
+    });
+
+    it('should fire a "stop" event', function () {
+      var called = false;
+      var tween = TinyTween.create().on('stop', function (event) {
+        called = true;
+      });
+      tween.start();
+      tween.stop();
+      expect(called).toBeTruthy();
     });
   });
 
